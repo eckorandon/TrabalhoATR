423d3b6057d9c250049ee2e2c5689aa9d1bb7580
Estevão Coelho Kiel de Oliveira <estevaokiel@gmail.com>
2021-07-20 21:21:27 -0300

    Update threads 5 e 6
    Tarefa de exibicao de dados de processo e de alarmes agora imprime na tela a estrutura generica em que os dados serao apresentados

--- TrabalhoPratico/TrabalhoPratico.cpp
+++ TrabalhoPratico/TrabalhoPratico.cpp
@@ -1,40 +1,51 @@
-//  UNIVERSIDADE FEDERAL DE MINAS GERAIS
-// 
-//  Trabalho prático
-//  Automação em tempo real (ELT012)
-//  
-//  Professor: 
-//  Luiz Themystokliz S. Mendes
-// 
-//  Alunos:
-//  Estevao Coelho Kiel de Oliveira - 2016119416
-//  Italo Jose Dias - 2017002121 
+/* =================================================================================================== */
+/*  CABECALHO*/
+/*
+    UNIVERSIDADE FEDERAL DE MINAS GERAIS
+ 
+    Trabalho pratico
+    Automacao em tempo real (ELT012)
+  
+    Professor: 
+    Luiz Themystokliz S. Mendes
+ 
+    Alunos:
+    Estevao Coelho Kiel de Oliveira - 2016119416
+    Italo Jose Dias - 2017002121 
+*/
 
-//  Para que a biblioteca Pthreads funcione corretamente e necessario tomar alguns cuidados
-//  
-//  1.  A biblioteca Pthreads 2.9.1 deve estar instalada no seu computador
-//      em C:\Program Files\pthreads-w32-2-9-1-release
-// 
-//  2.  No Visual studio Comunity Edition selecione
-//      Project -> Properties -> Configuration Properties -> C/C++ -> General
-//      e defina em "Additional Include Directories" o diretório onde encontram-se os
-//      "header files" da distribuicao pthreads em
-//      C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\include
-//
-//  3.  Agora selecione Project -> Properties -> Configuration Properties -> Linker -> General
-//      e defina em "Additional Library Directories" o diretorio onde se encontra
-//      a biblioteca Pthreads (extensão .LIB) em
-//      C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\lib\x86
-// 
-//  4.  Depois selecione Project -> Properties -> Configuration Properties -> Linker -> Input
-//      e declare a biblioteca "pthreadVC2.lib" em "Additional Dependencies"
-// 
-//  5.  Por fim, selecione Project -> Properties -> Configuration Properties -> Debugging 
-//      e entao preencha o item "Environment" com
-//      PATH=C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\dll\x86
+/* =================================================================================================== */
+/*  INTRUCOES PARA UTILIZACAO DA BIBLIOTECA PTHREAD*/
+/*
+    Para que a biblioteca Pthreads funcione corretamente e necessario tomar alguns cuidados
+  
+    1.  A biblioteca Pthreads 2.9.1 deve estar instalada no seu computador
+        em C:\Program Files\pthreads-w32-2-9-1-release
+ 
+    2.  No Visual studio Comunity Edition selecione
+        Project -> Properties -> Configuration Properties -> C/C++ -> General
+        e defina em "Additional Include Directories" o diretório onde encontram-se os
+        "header files" da distribuicao pthreads em
+        C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\include
 
-//DEFINICAO DE ALGUNS PARAMETROS
+    3.  Agora selecione Project -> Properties -> Configuration Properties -> Linker -> General
+        e defina em "Additional Library Directories" o diretorio onde se encontra
+        a biblioteca Pthreads (extensão .LIB) em
+        C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\lib\x86
+ 
+    4.  Depois selecione Project -> Properties -> Configuration Properties -> Linker -> Input
+        e declare a biblioteca "pthreadVC2.lib" em "Additional Dependencies"
+ 
+    5.  Por fim, selecione Project -> Properties -> Configuration Properties -> Debugging 
+        e entao preencha o item "Environment" com
+        PATH=C:\Program Files\pthreads-w32-2-9-1-release\Pre-built.2\dll\x86
+*/
+
+/* =================================================================================================== */
+/*  DEFINE AREA*/
+
 #define HAVE_STRUCT_TIMESPEC
+#define _CHECKERROR	    1				                            // Ativa funcao CheckForError
 #define RAM             100
 #define DISC            200
 
@@ -46,23 +57,24 @@
 #define	C_KEY			99
 #define	ESC_KEY			27
 
-#pragma warning(disable:4996)
+/* =================================================================================================== */
+/*  INCLUDE AREA*/
 
-//INCLUSAO DE BIBLIOTECAS ADICIONAIS
 #include <iostream>
 #include <windows.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <errno.h>
 #include <signal.h>
-#include <conio.h>		// _getch()
-#include <math.h>       // pow()
-#include <string.h>     // strcpy()
+#include <conio.h>		                                          // _getch()
+#include <math.h>                                                 // pow()
+#include <string.h>                                               // strcpy()
 #include <time.h>
+#include "CheckForError.h"
 
-// DECLARACAO DO PROTOTIPO DE FUNCAO DAS THREADS SECUNDARIAS
-// (void* arg)
-// trocar 0 por index
+/* =================================================================================================== */
+/*  DECLARACAO DO PROTOTIPO DE FUNCAO DAS THREADS SECUNDARIAS*/
+
 void* LeituraSDCD(void* arg);
 void* LeituraPIMS(void* arg);
 void* CapturaDados(void* arg);
@@ -70,27 +82,30 @@ void* CapturaAlarmes(void* arg);
 void* ExibicaoDados(void* arg);
 void* ExibicaoAlarmes(void* arg);
 
-//DECLARACAO DE VARIAVEIS
-char RamBuffer[RAM];
-char DiscBuffer[DISC];
+/* =================================================================================================== */
+/*  DECLARACAO DE VARIAVEIS GLOBAIS*/
 
+char RamBuffer[RAM][52];
+char DiscBuffer[DISC][52];
 
-//THREAD PRIMARIA
-//AQUI ACONTECE A TAREFA DE TRATAMENTO DO TECLADO E A IMPLEMENTACAO DA LISTA CIRCULAR
+/* =================================================================================================== */
+/*  THREAD PRIMARIA*/
+/*  CRIACAO DAS THREADS E TRATAMENTO DO TECLADO*/
+
 int main() {
 
     pthread_t hLeituraSDCD, hLeituraPIMS, hCapturaDados, hCapturaAlarmes, hExibicaoDados, hExibicaoAlarmes;
-    int status;
-    int i;
-    int key;
 
-    //CRIACAO DAS THREADS SECUNDARIAS
-    i = 1;
+/*  DECLARACAO VARIAVEIS LOCAIS DA MAIN*/
+    int status, i, key;
+
+/*  CRIACAO DAS THREADS SECUNDARIAS*/
+    /*i = 1;
     status = pthread_create(&hLeituraSDCD, NULL, LeituraSDCD, (void*)i);
     if (!status) printf("Thread %d criada com Id= %0x \n", i, (int)&hLeituraSDCD);
     else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);
 
-    /*i = 2;
+    i = 2;
     status = pthread_create(&hLeituraPIMS, NULL, LeituraPIMS, (void*)i);
     if (!status) printf("Thread %d criada com Id= %0x \n", i, (int)&hLeituraPIMS);
     else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);
@@ -104,18 +119,18 @@ int main() {
     status = pthread_create(&hCapturaAlarmes, NULL, CapturaAlarmes, (void*)i);
     if (!status) printf("Thread %d criada com Id= %0x \n", i, (int)&hCapturaAlarmes);
     else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);
-
+    
     i = 5;
     status = pthread_create(&hExibicaoDados, NULL, ExibicaoDados, (void*)i);
     if (!status) printf("Thread %d criada com Id= %0x \n", i, (int)&hExibicaoDados);
     else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);
-
+    */
     i = 6;
     status = pthread_create(&hExibicaoAlarmes, NULL, ExibicaoAlarmes, (void*)i);
     if (!status) printf("Thread %d criada com Id= %0x \n", i, (int)&hExibicaoAlarmes);
-    else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);*/
+    else printf("Erro na criacao da thread %d! Codigo = %d\n", i, status);
 
-    //INPUTS DO TECLADO
+/*  TRATAMENTO INPUTS DO TECLADO*/
     while (TRUE) {
         key = _getch();
         switch (key) {
@@ -143,14 +158,20 @@ int main() {
         default:
             break;;
         }
-    }
+    } /*fim do while*/
 
     return EXIT_SUCCESS;
-}
+} /*fim da funcao main*/
 
-//THREAD DE LEITURA DO SCDC
-//Falta fazer variaveis com os valores de campos
-//Falta implementar a comunicacao da memoria com os mutex
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE LEITURA SDCD*/
+/*  GERACAO DE VALORES E GRAVACAO DOS MESMOS EM MEMORIA RAM*/
+/*
+    [] FALTA FAZER AS VARIAVEIS COM OS VALORES DOS CAMPOS
+    [] FALTA GRAVACAO DE VALORES NA MEMORIA CIRCULAR COM USO DE MUTEX
+    [] FALTA COMENTAR NO PADRAO
+*/
+
 void* LeituraSDCD(void* arg) {
     
     int     index = (int)arg,
@@ -299,8 +320,14 @@ void* LeituraSDCD(void* arg) {
     return (void*)index;
 }
 
-//THREAD DE LEITURA DO PIMS
-//Falta implementar a comunicacao da memoria com os mutex
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE LEITURA PIMS*/
+/*  GERACAO DE VALORES E GRAVACAO DOS MESMOS EM MEMORIA RAM*/
+/*
+    [] FALTA GRAVACAO DE VALORES NA MEMORIA CIRCULAR COM USO DE MUTEX
+    [] FALTA COMENTAR NO PADRAO
+*/
+
 void* LeituraPIMS(void* arg) {
     int     index = (int)arg,
             k = 0, i = 0, l = 0,
@@ -418,7 +445,13 @@ void* LeituraPIMS(void* arg) {
     return (void*)index;
 }
 
-//THREAD DE CAPTURA DE DADOS DO PROCESSO
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE CAPTURA DE DADOS DO PROCESSO*/
+/*  XXXX*/
+/*
+    [] FALTA TUDO
+*/
+
 void* CapturaDados(void* arg) {
     int index, i;
 
@@ -436,7 +469,13 @@ void* CapturaDados(void* arg) {
     return (void*)index;
 }
 
-//THREAD DE CAPTURA DE ALARMES
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE CAPTURA DE ALARMES
+/*  XXXX*/
+/*
+    [] FALTA TUDO
+*/
+
 void* CapturaAlarmes(void* arg) {
     int index, i;
 
@@ -454,15 +493,88 @@ void* CapturaAlarmes(void* arg) {
     return (void*)index;
 }
 
-//THREAD DE EXIBICAO DE DADOS DO PROCESSO
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE EXIBICAO DE DADOS DO PROCESSO*/
+/*  XXXX*/
+/*
+    [] FALTA TUDO
+*/
+
 void* ExibicaoDados(void* arg) {
-    int index, i;
 
-    index = (int)arg;
+/*  DECLARACAO DE VARIAVEIS LOCAIS*/
+    int i, index = (int)arg;
 
-    for (i = 0; i < 100000; ++i) {
-        printf("%d ", index);
-        Sleep(1);	// delay de 1 ms
+    char    SDCD[75];
+
+/*  PREENCHENDO VALORES GENERICOS DOS DADOS QUE SERAO EXIBIDOS NO TERMINAL A*/
+    for (int j = 0; j < 71; j++) {
+        if (j == 4 || j == 14 || j == 17 || j == 20 || j == 31 || j == 48 || j == 60 || j == 74) {
+            SDCD[j] = ':';
+        }
+        else if (j == 9 || j == 27 || j == 42 || j == 57) {
+            SDCD[j] = ' ';
+        }
+        else if (j == 11 || j == 46 || j == 71 || j == 73) {
+            SDCD[j] = 'O';
+        }
+        else if (j == 13 || j == 29 || j == 44) {
+            SDCD[j] = 'A';
+        }
+        else if (j == 2 || j == 59) {
+            SDCD[j] = 'E';
+        }
+        else if (j == 58) {
+            SDCD[j] = 'U';
+        }
+        else if (j == 0) {
+            SDCD[j] = 'N';
+        }
+        else if (j == 1) {
+            SDCD[j] = 'S';
+        }
+        else if (j == 3) {
+            SDCD[j] = 'Q';
+        }
+        else if (j == 10) {
+            SDCD[j] = 'H';
+        }
+        else if (j == 12 || j == 47) {
+            SDCD[j] = 'R';
+        }
+        else if (j == 23) {
+            SDCD[j] = '.';
+        }
+        else if (j == 28) {
+            SDCD[j] = 'T';
+        }
+        else if (j == 30) {
+            SDCD[j] = 'G';
+        }
+        else if (j == 43) {
+            SDCD[j] = 'V';
+        }
+        else if (j == 45) {
+            SDCD[j] = 'L';
+        }
+        else if (j == 70) {
+            SDCD[j] = 'M';
+        }
+        else if (j == 72) {
+            SDCD[j] = 'D';
+        }
+        else
+            SDCD[j] = '#';
+    }
+
+    while (true) {
+        
+/*      IMPRIME A MENSAGEM do SDCD*/
+        for (int j = 0; j < 52; j++) {
+            printf("%c", SDCD[j]);
+        }
+
+        printf("\n");
     }
 
     pthread_exit((void*)index);
@@ -472,15 +584,84 @@ void* ExibicaoDados(void* arg) {
     return (void*)index;
 }
 
-//THREAD DE EXIBICAO DE ALARMES
+/* =================================================================================================== */
+/*  THREAD SECUNDARIA DE EXIBICAO DE ALARMES*/
+/*  XXXX*/
+/*
+    [] FALTA TUDO
+*/
+
 void* ExibicaoAlarmes(void* arg) {
-    int index, i;
+    /*  DECLARACAO DE VARIAVEIS LOCAIS*/
+    int i, index = (int)arg;
 
-    index = (int)arg;
+    char    PIMS[54];
 
-    for (i = 0; i < 100000; ++i) {
-        printf("%d ", index);
-        Sleep(1);	// delay de 1 ms
+    /*  PREENCHENDO VALORES GENERICOS DOS DADOS QUE SERAO EXIBIDOS NO TERMINAL A*/
+    for (int j = 0; j < 71; j++) {
+        if (j == 2 || j == 5 || j == 13 || j == 30 || j == 40 || j == 48) {
+            PIMS[j] = ':';
+        }
+        else if (j == 8 || j == 20 || j == 23 || j == 35 || j == 43) {
+            PIMS[j] = ' ';
+        }
+        else if (j == 24 || j == 26 || j == 38) {
+            PIMS[j] = 'A';
+        }
+        else if (j == 11 || j == 29 || j == 46) {
+            PIMS[j] = 'E';
+        }
+        else if (j == 39) {
+            PIMS[j] = 'U';
+        }
+        else if (j == 9) {
+            PIMS[j] = 'N';
+        }
+        else if (j == 10) {
+            PIMS[j] = 'S';
+        }
+        else if (j == 12) {
+            PIMS[j] = 'Q';
+        }
+        else if (j == 27 || j == 37 || j == 45) {
+            PIMS[j] = 'R';
+        }
+        else if (j == 36) {
+            PIMS[j] = 'G';
+        }
+        else if (j == 47) {
+            PIMS[j] = 'V';
+        }
+        else if (j == 25) {
+            PIMS[j] = 'L';
+        }
+        else if (j == 28) {
+            PIMS[j] = 'M';
+        }
+        else if (j == 22) {
+            PIMS[j] = 'D';
+        }
+        else if (j == 21) {
+            PIMS[j] = 'I';
+        }
+        else if (j == 36) {
+            PIMS[j] = 'G';
+        }
+        else if (j == 44) {
+            PIMS[j] = 'P';
+        }
+        else
+            PIMS[j] = '#';
+    }
+
+    while (true) {
+
+        /*      IMPRIME A MENSAGEM do PIMS*/
+        for (int j = 0; j < 52; j++) {
+            printf("%c", PIMS[j]);
+        }
+
+        printf("\n");
     }
 
     pthread_exit((void*)index);
